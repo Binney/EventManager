8973182c4174196f79ff619c40237b54b27d997b
Asif Hafeez <Asif.Hafeez@softwire.com>
2017-01-25 14:21:02 +0000

    routing fixed

--- EventManager/App_Start/RouteConfig.cs
+++ EventManager/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace EventManager
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 name: "Default",
---
+++ EventManager/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+
+namespace EventManager.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Time { get; set; }
+    }
+
+    public class EventDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+    }
+
+}
