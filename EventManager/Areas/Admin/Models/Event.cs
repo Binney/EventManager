a3d01d0f965456f70347ab0e8e5a6d728733b646
Asif Hafeez <Asif.Hafeez@softwire.com>
2017-01-27 12:01:30 +0000

    added one-to-zero-or-one relationship

--- EventManager/Areas/Admin/Models/Event.cs
+++ EventManager/Areas/Admin/Models/Event.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
+using EventManager.Models;
 
 namespace EventManager.Areas.Admin.Models
 {
     public class Event
     {
-        public int ID { get; set; }
+        public int EventId { get; set; }
         [Required,StringLength(60)]
         public string Name { get; set; }
         [DataType(DataType.Date),DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
         [DataType(DataType.Time),DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
         public TimeSpan Time { get; set; }
+        public virtual Booking Booking { get; set; }
     }
 
     public class EventDbContext : DbContext
---
+++ EventManager/Models/Booking.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using EventManager.Areas.Admin.Models;
+
+namespace EventManager.Models
+{
+    public class Booking
+    {
+        [ForeignKey("Event")]
+        public int BookingId { get; set; }
+        [Required, DataType(DataType.EmailAddress)]
+        [Display(Name = "First Guest")]
+        public string Guest1 { get; set; }
+        [Required, DataType(DataType.EmailAddress)]
+        [Display(Name = "Second Guest")]
+        public string Guest2 { get; set; }
+        [Required, DataType(DataType.EmailAddress)]
+        [Display(Name = "Third Guest")]
+        public string Guest3 { get; set; }
+        public virtual Event Event { get; set; }
+    }
+
+    public class BookingDbContext : DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
