24f3a5ed74ab8f57449a594472de4cffce777c16
Asif Hafeez <Asif.Hafeez@softwire.com>
2017-01-30 09:56:19 +0000

    added active field to invitations

--- EventManager/Areas/Admin/Models/Invitation.cs
+++ EventManager/Areas/Admin/Models/Invitation.cs
@@ -1,10 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace EventManager.Areas.Admin.Models
 {
     public class Invitation
     {
+        public Invitation()
+        {
+            Active = true;
+        }
         public int ID { get; set; }
         [Required, StringLength(60, MinimumLength = 3)]
         public string Name { get; set; }
@@ -13,5 +18,6 @@ namespace EventManager.Areas.Admin.Models
         [Required, StringLength(12, MinimumLength = 12, ErrorMessage = "Code must be 12 characters long")] 
         [RegularExpression(@"^[A-Z0-9]*$", ErrorMessage = "Please use only uppercase letters and numbers")]
         public string InvitationCode { get; set; }
+        public bool Active { get; set; }
     }
 }
