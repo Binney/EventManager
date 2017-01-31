03a8c49db285898b0381173f6a38d3afd3313a1d
Asif Hafeez <Asif.Hafeez@softwire.com>
2017-01-31 12:41:59 +0000

    started styling

--- EventManager/App_Start/BundleConfig.cs
+++ EventManager/App_Start/BundleConfig.cs
@@ -17,15 +17,22 @@ namespace EventManager
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/Chart.js",
+                      "~/Scripts/classie.js",
+                      "~/Scripts/main.js",
+                      "~/Scripts/smoothscroll.js",
+                      "~/Scripts/toggle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", 
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/main.css"));
         }
     }
 }
