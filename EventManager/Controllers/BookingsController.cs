e50e079aa434c27d738bb2bb9f65bdb2a64dd858
Asif Hafeez <Asif.Hafeez@softwire.com>
2017-01-31 14:44:12 +0000

    finished styling user interface

--- EventManager/Controllers/BookingsController.cs
+++ EventManager/Controllers/BookingsController.cs
@@ -19,14 +19,6 @@ namespace EventManager.Controllers
     public class BookingsController : Controller
     {
         private EventManagerDbContext db = new EventManagerDbContext();
-
-        // GET: Bookings
-        public ActionResult Index()
-        {
-            var bookings = db.Bookings.Include(b => b.Event);
-            return View(bookings.ToList());
-        }
-
         // GET: Bookings/Details/5
         public ActionResult Details(int? id)
         {
@@ -86,39 +78,6 @@ namespace EventManager.Controllers
             return View(booking);
         }
 
-        // GET: Bookings/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Booking booking = db.Bookings.Find(id);
-            if (booking == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.EventId = new SelectList(db.Events, "EventId", "Name", booking.EventId);
-            return View(booking);
-        }
-
-        // POST: Bookings/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "EventId,Guest1,Guest2,Guest3")] Booking booking)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(booking).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.EventId = new SelectList(db.Events, "EventId", "Name", booking.EventId);
-            return View(booking);
-        }
-
         // GET: Bookings/Delete/5
         public ActionResult Delete(int? id)
         {
@@ -150,7 +109,7 @@ namespace EventManager.Controllers
                 db.Bookings.Remove(booking);
                 db.SaveChanges();
             }
-            return RedirectToAction("Index");
+            return RedirectToAction("Upcoming", "Home");
         }
 
         protected override void Dispose(bool disposing)
